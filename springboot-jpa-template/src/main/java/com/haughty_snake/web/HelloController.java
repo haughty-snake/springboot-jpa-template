7844860300f744116354a8593927d19624947419
ilho, Jeong <ihjeong.dev@gmail.com>
2020-10-04 14:12:02 +0900

    Rest API sample

---
+++ springboot-jpa-template/src/main/java/com/haughty_snake/web/HelloController.java
@@ -0,0 +1,14 @@
+package com.haughty_snake.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+	
+}
---
+++ springboot-jpa-template/src/test/java/com/haughty_snake/web/HelloControllerTest.java
@@ -0,0 +1,35 @@
+package com.haughty_snake.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE)
+        }
+)
+public class HelloControllerTest {
+
+	@Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
